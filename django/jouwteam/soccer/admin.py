013d7d9f34bfa50061a8c179c918e7513a516c09
Gizemsaz <g.sazak@live.nl>
2019-10-15 17:08:05 +0200

    database models

--- django/jouwteam/soccer/admin.py
+++ django/jouwteam/soccer/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import (registration, inloggen)
+from .models import trainer, spelerlogin, speler, aanwezigheid, tactieken, agenda, notitie, oefeningen, wedstrijduitslag
+
 
 # Register your models here.
-admin.site.register(registration)
-admin.site.register(inloggen)
+admin.site.register(trainer)
+admin.site.register(spelerlogin)
+admin.site.register(speler)
+admin.site.register(aanwezigheid)
+admin.site.register(tactieken)
+admin.site.register(agenda)
+admin.site.register(notitie)
+admin.site.register(oefeningen)
+admin.site.register(wedstrijduitslag)
