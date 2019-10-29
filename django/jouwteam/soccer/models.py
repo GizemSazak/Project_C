9d113a7f1a9c4527d19a2279b5bd6cbfbe280963
saloua <saloua1998@live.nl>
2019-10-29 09:27:39 +0100

    added django gitignore

--- django/jouwteam/soccer/models.py
+++ django/jouwteam/soccer/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-class registration(models.Model):
-    username = models.CharField(max_length=200,default='False')
-class inloggen(models.Model):
-    username = models.CharField(max_length=200,default='False')
-# Create your models here.
 
+
+class registration(models.Model):
+    username = models.CharField(max_length=200, default='False')
+
+
+class inloggen(models.Model):
+    username = models.CharField(max_length=200, default='False')
+# Create your models here.
