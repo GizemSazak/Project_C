72795149e6a0c66fe1924af32e45943de2f7258a
saloua <saloua1998@live.nl>
2019-11-15 07:44:05 +0100

    Tried to convert function to class

--- src/Uitslagen.js
+++ src/Uitslagen.js
@@ -5,42 +5,59 @@ import Check from './components/Menu/Check';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
 
-
-
-
-
 function Uitslagen() {
 
-  const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([])
 
     useEffect(() => {
         axios.get('http://localhost:3001/api/wedstrijduitslag')
-        .then(res => {
-            console.log(res)
-        })
-        .catch()
+            .then(res => {
+                console.log(res)
+            })
+            .catch()
     })
-
 
     return (
         <div className="App">
             <h1 className='titleOefeningen'>Wedstrijduistslagen</h1>
-            
             <ul>
-                {posts.map(post => <li>
-                    {post.thuis} {post.stand} {post.uit}
+                {posts.map(post =>
+                    <li>
+                        {post.thuis} {post.stand} {post.uit}
                     </li>
-                    )}
+                )}
             </ul>
-        <Check />
-            
+            <Check />
         </div>
-        
     );
-
-    
-          
 }
+export default Uitslagen;
+// class Uitslagen extends React.Component {
+//     render() {
+//         const [posts, setPosts] = useState([])
 
-export default Uitslagen
+//         // useEffect(() => {
+//         //     axios.get('http://localhost:3001/api/wedstrijduitslag')
+//         //         .then(res => {
+//         //             console.log(res)
+//         //         })
+//         //         .catch()
+//         // })
+
+//         return (
+//             <div className="App">
+//                 <h1 className='titleOefeningen'>Wedstrijduistslagen</h1>
+//                 {/* <ul>
+//                     {posts.map(post => <li>
+//                         {post.thuis} {post.stand} {post.uit}
+//                     </li>
+//                     )}
+//                 </ul> */}
+//                 <Check />
+//             </div>
+//         )
+//     }
+// }
+
+
 
