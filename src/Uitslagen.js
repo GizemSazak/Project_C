4f73544ec99f1d7806d141756a526ca8b0c6f1ea
Gizemsaz <g.sazak@live.nl>
2019-11-15 17:08:02 +0100

    tabel

--- src/Uitslagen.js
+++ src/Uitslagen.js
@@ -4,6 +4,7 @@ import './Uitslagen.css';
 import Check from './components/Menu/Check';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
+import { tsAnyKeyword } from '@babel/types';
 
 function Uitslagen() {
 
@@ -19,46 +20,43 @@ function Uitslagen() {
     })
 
     return (
-        <div>
+        <div className="App">
             <h1 className='titleOefeningen'>Wedstrijduistslagen</h1>
-            <ul>
-                {posts.map(post =>
-                    <li>
-                        {post.thuis} {post.stand} {post.uit}
-                    </li>
-                )}
-            </ul>
+            <div className="uitslagBody">
+                <tbody id="tt" >
+                            <tr >
+                                <th id="tableL" >#</th>
+                                <th id="tableR"  >Thuis</th>
+                                <th id="tableR" >Stand</th>
+                                <th id="tableR" >Uit</th>
+                            </tr>    
+                    {posts.map(function(post){
+                    return(
+                            <tr>
+                            <th id="tableL" >
+                                {post.id} 
+                            </th>   
+                            <td id="tableR" >
+                                {post.thuis} 
+                            </td>   
+                            <td id="tableR" >
+                                {post.stand} 
+                            </td>   
+                            <td id="tableR" >
+                                {post.uit} 
+                            </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+                 
+            </div>
             <Check />
         </div>
     );
 }
 export default Uitslagen;
-// class Uitslagen extends React.Component {
-//     render() {
-//         const [posts, setPosts] = useState([])
-
-//         // useEffect(() => {
-//         //     axios.get('http://localhost:3001/api/wedstrijduitslag')
-//         //         .then(res => {
-//         //             console.log(res)
-//         //         })
-//         //         .catch()
-//         // })
-
-//         return (
-//             <div className="App">
-//                 <h1 className='titleOefeningen'>Wedstrijduistslagen</h1>
-//                 {/* <ul>
-//                     {posts.map(post => <li>
-//                         {post.thuis} {post.stand} {post.uit}
-//                     </li>
-//                     )}
-//                 </ul> */}
-//                 <Check />
-//             </div>
-//         )
-//     }
-// }
+        
 
 
 
