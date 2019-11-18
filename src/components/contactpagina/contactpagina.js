5fd10cf0c09070123198e2ea6b91eef597e0d2b1
isti12345 <0953404@hr.nl>
2019-11-18 19:42:05 +0100

    Contact page with wrapper

--- src/components/contactpagina/contactpagina.js
+++ src/components/contactpagina/contactpagina.js
@@ -3,20 +3,22 @@ import './contactpagina.css'
 
 function Contact() {
     return (
-      <div className='contact'>
-        <h2>Contact gegevens</h2>
+      <div className='Contact_wrapper'>
+        <div className='contact'>
+        <h2 className='contact_gegevens'>Contact gegevens</h2>
           <div>
-            <section>Mijn team</section>
+            <section className='team'>Mijn team</section>
               <div>
                 <section>E-mail: 0953404@hr.nl</section>
                   <div>
                     <section>Mobiel: 0679534169</section>
                       <div>
                         <section>Adres: Wijnhaven 107</section>
-                      </div>
-                  </div>
-              </div>
-          </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+        </div>
       </div>
   
       
