b79f7fc1cf48db92785282849816350c570e46d5
Gizemsaz <g.sazak@live.nl>
2019-11-20 21:48:05 +0100

    functionaliteit verbeterd

--- src/uitslagtoevoegen.js
+++ src/uitslagtoevoegen.js
@@ -10,19 +10,21 @@ import addbutton from './addbutton.png'
 class uitslagtoevoegen extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      id: 0,
-      thuis: '',
-      stand: '',
-      uit: '',
-      verslag: ''
-    }
+    this.state = {id: 0}
+      this.state = {thuis: ''}
+      this.state = {stand: ''}
+      this.state = {uit: ''}
+      this.state = {verslag: ''}
+    
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+  updateInput(event) {
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   handleChange (event) {
-    let nam = event.target.name;
-    let val = event.target.value;
-    this.setState({[nam]: val});
+    const values = [this.state.id, this.state.thuis, this.state.stand, this.state.uit, this.state.verslag]
   }
 
 
@@ -54,13 +56,13 @@ class uitslagtoevoegen extends Component {
           <div className="standtoevoegenBody">
 
             <form useRef="dataForm" className="standForm">
-              <input id="formp1" name="id" type="text" placeholder="week" onChange={event => this.handleChange(event)} /><br></br>
-              <input id="formp1" name="thuis" type="text" placeholder="thuis" onChange={event => this.handleChange(event)} /><br></br>
-              <input id="formp1" name="uit" type="text" placeholder="uit" onChange={event => this.handleChange(event)} /><br></br>
-              <input id="formp1" name="stand" type="text" placeholder="stand" onChange={event => this.handleChange(event)} /><br></br>
-              <textarea cols="23" namee="verslag" rows="20" id="formp2" type="text" placeholder="verslag" onChange={event => this.handleChange(event)} ></textarea>
+              <input id="formp1" name="id" type="text" placeholder="week" className="Inputfield" onChange={this.updateInput} /><br></br>
+              <input id="formp1" name="thuis" type="text" placeholder="thuis" className="Inputfield" onChange={this.updateInput} /><br></br>
+              <input id="formp1" name="uit" type="text" placeholder="uit" className="Inputfield" onChange={this.updateInput}/><br></br>
+              <input id="formp1" name="stand" type="text" placeholder="stand" className="Inputfield" onChange={this.updateInput} /><br></br>
+              <textarea cols="23" name="verslag" rows="20" id="formp2" type="text" placeholder="verslag" className="Inputfield" onChange={this.updateInput} ></textarea>
               <br></br>
-              <Link to='./Uitslagen' refresh="true"><button onClick={() => this.addUitslag()}>Toevoegen</button></Link>
+              <Link to='./Uitslagen' refresh="true"><button className="SubmitBtn" onClick={()=> this.addUitslag()}>Toevoegen</button></Link>
             </form>
 
           </div>
